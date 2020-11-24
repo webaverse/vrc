2331a0955d50bbf7d9cd6b67db4d9416952ec90b
Avaer Kazmer <a@modules.io>
2020-11-23 21:31:43 -0500

    Steam debugging

--- lol.js
+++ lol.js
@@ -46,181 +46,200 @@ fs.writeFileSync('file-map.json', JSON.stringify(fileMap, null, 2));
 
 // fs.writeFileSync('temp.json', JSON.stringify(scene, null, 2));
 
+const meshCache = {};
+const _getMesh = guid => {
+  let entry = meshCache[guid];
+  if (entry === undefined) {
+    const meshFilePath = fileMap[guid];
+    if (meshFilePath) {
+      const o = YAML.parse(fs.readFileSync(meshFilePath, 'utf8'));
+      
+      // input example: A simple Quad
 
-const _parseMesh = o => {
-  // input example: A simple Quad
+      // field `_typelessdata`
+      // mesh = `000080bf000080bf0000000000000000000000000000803f00000000000000000000803f000080bf0000000000000000000000000000803f0000803f00000000000080bf0000803f0000000000000000000000000000803f000000000000803f0000803f0000803f0000000000000000000000000000803f0000803f0000803f`;
+      // field `m_IndexBuffer`
+      // index = `0000010002000300`;
 
-  // field `_typelessdata`
-  // mesh = `000080bf000080bf0000000000000000000000000000803f00000000000000000000803f000080bf0000000000000000000000000000803f0000803f00000000000080bf0000803f0000000000000000000000000000803f000000000000803f0000803f0000803f0000000000000000000000000000803f0000803f0000803f`;
-  // field `m_IndexBuffer`
-  // index = `0000010002000300`;
+      // normalize
+      const mesh = normalizeString(o.Mesh.m_VertexData._typelessdata);
+      /* if (!o.Mesh.m_IndexBuffer.replace) {
+        const index = o.Mesh.m_IndexBuffer.toString(8).padStart(24, '0');
+        const indices = [];
+        for (let ii = 0; ii < index.length / 4; ++ii) {
+          indices.push(swap16(parseInt(`0x` + index.substr(ii * 4, 4))));
+        };
+        console.warn('no index buffer', [o.Mesh.m_IndexBuffer, index], indices);
+        throw 'lol';
+      } */
+      const index = normalizeString(typeof o.Mesh.m_IndexBuffer === 'string' ? o.Mesh.m_IndexBuffer : padTo4(o.Mesh.m_IndexBuffer));
+      const channels = o.Mesh.m_VertexData.m_Channels;
+      const numVertices = o.Mesh.m_VertexData.m_VertexCount;
+      const streams = [];
+      for (const channel of channels) {
+        const {stream, offset, format, dimension} = channel;
+        if (dimension === 0) {
+          continue;
+        }
+        let s = streams.find(s => s.stream === stream);
+        if (!s) {
+          s = [];
+          s.stream = stream;
+          streams.push(s);
+        }
+        s.push({
+          stream,
+          offset,
+          format,
+          dimension,
+        });
+      }
+      /* const strides = streams.map(slots => {
+        let result = 0;
+        for (const slot of slots) {
+          result += slot.dimension;
+        }
+        return result;
+      }); */
+      // console.log('got streams', streams);
 
-  // normalize
-  const mesh = normalizeString(o.Mesh.m_VertexData._typelessdata);
-  /* if (!o.Mesh.m_IndexBuffer.replace) {
-    const index = o.Mesh.m_IndexBuffer.toString(8).padStart(24, '0');
-    const indices = [];
-    for (let ii = 0; ii < index.length / 4; ++ii) {
-      indices.push(swap16(parseInt(`0x` + index.substr(ii * 4, 4))));
-    };
-    console.warn('no index buffer', [o.Mesh.m_IndexBuffer, index], indices);
-    throw 'lol';
-  } */
-  const index = normalizeString(typeof o.Mesh.m_IndexBuffer === 'string' ? o.Mesh.m_IndexBuffer : padTo4(o.Mesh.m_IndexBuffer));
-  const channels = o.Mesh.m_VertexData.m_Channels;
-  const numVertices = o.Mesh.m_VertexData.m_VertexCount;
-  const streams = [];
-  for (const channel of channels) {
-    const {stream, offset, format, dimension} = channel;
-    if (dimension === 0) {
-      continue;
-    }
-    let s = streams.find(s => s.stream === stream);
-    if (!s) {
-      s = [];
-      s.stream = stream;
-      streams.push(s);
-    }
-    s.push({
-      stream,
-      offset,
-      format,
-      dimension,
-    });
-  }
-  /* const strides = streams.map(slots => {
-    let result = 0;
-    for (const slot of slots) {
-      result += slot.dimension;
-    }
-    return result;
-  }); */
-  // console.log('got streams', streams);
+      function padTo4(n) {
+        return n.toString(8).padStart(24, '0');
+      }
 
-  function padTo4(n) {
-    return n.toString(8).padStart(24, '0');
-  }
+      function normalizeString(str) {
+        return str.replace(`\\`, "").replace(`\n`, "").replace(` `, "");
+      };
 
-  function normalizeString(str) {
-    return str.replace(`\\`, "").replace(`\n`, "").replace(` `, "");
-  };
+      function hex2float(num) {
+        let sign = (num & 0x80000000) ? -1 : 1;
+        let exponent = ((num >> 23) & 0xff) - 127;
+        let mantissa = 1 + ((num & 0x7fffff) / 0x7fffff);
+        return sign * mantissa * Math.pow(2, exponent);
+      }
+      
+      function hex2halffloat(num) { // XXX
+        let sign = (num & 0x80000000) ? -1 : 1;
+        let exponent = ((num >> 23) & 0xff) - 127;
+        let mantissa = 1 + ((num & 0x7fffff) / 0x7fffff);
+        return sign * mantissa * Math.pow(2, exponent);
+      }
 
-  function hex2float(num) {
-    let sign = (num & 0x80000000) ? -1 : 1;
-    let exponent = ((num >> 23) & 0xff) - 127;
-    let mantissa = 1 + ((num & 0x7fffff) / 0x7fffff);
-    return sign * mantissa * Math.pow(2, exponent);
-  }
-  
-  function hex2halffloat(num) { // XXX
-    let sign = (num & 0x80000000) ? -1 : 1;
-    let exponent = ((num >> 23) & 0xff) - 127;
-    let mantissa = 1 + ((num & 0x7fffff) / 0x7fffff);
-    return sign * mantissa * Math.pow(2, exponent);
-  }
+      function swap16(val) {
+        return ((val & 0xFF) << 8) | ((val >> 8) & 0xFF);
+      };
 
-  function swap16(val) {
-    return ((val & 0xFF) << 8) | ((val >> 8) & 0xFF);
-  };
+      function swap32(val) {
+        return (
+          ((val << 24) & 0xff000000) |
+          ((val << 8) & 0xff0000) |
+          ((val >> 8) & 0xff00) |
+          ((val >> 24) & 0xff)
+        );
+      };
 
-  function swap32(val) {
-    return (
-      ((val << 24) & 0xff000000) |
-      ((val << 8) & 0xff0000) |
-      ((val >> 8) & 0xff00) |
-      ((val >> 24) & 0xff)
-    );
-  };
-
-  // process `_typelessdata` field
-  /* const vertices = [];
-  const normals = [];
-  const tangents = [];
-  const uvs = []; */
-  const slices = [];
-  for (let ii = 0; ii < mesh.length / 8; ++ii) {
-    let slice = parseFloat(hex2float(swap32(parseInt(`0x` + mesh.substr(ii * 8, 8)))).toFixed(4));
-    slices.push(slice);
-  };
-  const buffers = [];
-  for (const stream of streams) {
-    const streamBuffer = [];
-    for (let i = 0; i < stream.length; i++) {
-      streamBuffer.push([]);
-    }
-    buffers.push(streamBuffer);
-  }
-  
-  /* delete o.Mesh.m_VertexData._typelessdata;
-  delete o.Mesh.m_IndexBuffer;
-  console.log('got streams', JSON.stringify(o.Mesh.m_VertexData, null, 2)); */
-  
-  let offset = 0;
-  for (let streamIndex = 0; streamIndex < streams.length; streamIndex++) {
-    const stream = streams[streamIndex];
-    // const stride = strides[streamIndex];
-    for (let j = 0; j < numVertices; j++) {
-      for (let slotIndex = 0; slotIndex < stream.length; slotIndex++) {
-        const slot = stream[slotIndex];
-        for (let k = 0; k < slot.dimension; k++) {
-          buffers[streamIndex][slotIndex].push(slices[offset++]);
+      // process `_typelessdata` field
+      /* const vertices = [];
+      const normals = [];
+      const tangents = [];
+      const uvs = []; */
+      const slices = [];
+      for (let ii = 0; ii < mesh.length / 8; ++ii) {
+        let slice = parseFloat(hex2float(swap32(parseInt(`0x` + mesh.substr(ii * 8, 8)))).toFixed(4));
+        slices.push(slice);
+      };
+      const buffers = [];
+      for (const stream of streams) {
+        const streamBuffer = [];
+        for (let i = 0; i < stream.length; i++) {
+          streamBuffer.push([]);
+        }
+        buffers.push(streamBuffer);
+      }
+      
+      /* delete o.Mesh.m_VertexData._typelessdata;
+      delete o.Mesh.m_IndexBuffer;
+      console.log('got streams', JSON.stringify(o.Mesh.m_VertexData, null, 2)); */
+      
+      let offset = 0;
+      for (let streamIndex = 0; streamIndex < streams.length; streamIndex++) {
+        const stream = streams[streamIndex];
+        // const stride = strides[streamIndex];
+        for (let j = 0; j < numVertices; j++) {
+          for (let slotIndex = 0; slotIndex < stream.length; slotIndex++) {
+            const slot = stream[slotIndex];
+            for (let k = 0; k < slot.dimension; k++) {
+              buffers[streamIndex][slotIndex].push(slices[offset++]);
+            }
+          }
         }
       }
+      console.log('done', offset, slices.length);
+      /* for (let ii = 0; ii < slices.length / 12; ++ii) {
+        let offset = ii * 12;
+        let v0 = slices[offset + 0];
+        let v1 = slices[offset + 1];
+        let v2 = slices[offset + 2];
+        let n0 = slices[offset + 3];
+        let n1 = slices[offset + 4];
+        let n2 = slices[offset + 5];
+        let u0 = slices[offset + 6];
+        let u1 = slices[offset + 7];
+        let t0 = slices[offset + 8];
+        let t1 = slices[offset + 9];
+        let t2 = slices[offset + 10];
+        let t3 = slices[offset + 11];
+        vertices.push(v0, v1, v2);
+        normals.push(n0, n1, n2);
+        uvs.push(u0, u1);
+        tangents.push(t0, t1, t2, t3);
+      }; */
+
+      const [vertices, normals, tangents, uvs] = buffers.flat();
+
+      // process `m_IndexBuffer` field
+      const indices = [];
+      for (let ii = 0; ii < index.length / 4; ++ii) {
+        indices.push(swap16(parseInt(`0x` + index.substr(ii * 4, 4))));
+      };
+
+      entry = {
+        vertices,
+        normals,
+        uvs,
+        tangents,
+        indices,
+      };
+      meshCache[guid] = entry;
+    } else {
+      entry = null;
+      meshCache[guid] = entry;
     }
   }
-  console.log('done', offset, slices.length);
-  /* for (let ii = 0; ii < slices.length / 12; ++ii) {
-    let offset = ii * 12;
-    let v0 = slices[offset + 0];
-    let v1 = slices[offset + 1];
-    let v2 = slices[offset + 2];
-    let n0 = slices[offset + 3];
-    let n1 = slices[offset + 4];
-    let n2 = slices[offset + 5];
-    let u0 = slices[offset + 6];
-    let u1 = slices[offset + 7];
-    let t0 = slices[offset + 8];
-    let t1 = slices[offset + 9];
-    let t2 = slices[offset + 10];
-    let t3 = slices[offset + 11];
-    vertices.push(v0, v1, v2);
-    normals.push(n0, n1, n2);
-    uvs.push(u0, u1);
-    tangents.push(t0, t1, t2, t3);
-  }; */
-
-  const [vertices, normals, tangents, uvs] = buffers.flat();
-
-  // process `m_IndexBuffer` field
-  const indices = [];
-  for (let ii = 0; ii < index.length / 4; ++ii) {
-    indices.push(swap16(parseInt(`0x` + index.substr(ii * 4, 4))));
-  };
-
-  return {
-    vertices,
-    normals,
-    uvs,
-    tangents,
-    indices,
-  };
+  return entry;
 };
-const _parseMaterial = m => {
-  const matFilePath = fileMap[m.guid];
-  const matFile = YAML.parse(fs.readFileSync(matFilePath, 'utf8'));
-  console.log('got material', matFile);
-  const textures = matFile.Material.m_SavedProperties.m_TexEnvs.map(o => {
-    const k = Object.keys(o);
-    o = o[k];
-    const {m_Texture} = o;
-    if (m_Texture.guid) {
-      const textureFilePath = fileMap[m_Texture.guid];
-      return textureFilePath || null;
-    } else {
-      return null;
-    }
-  }).filter(m => m !== null);
-  return textures;
+const materialCache = {};
+const _getMaterial = m => {
+  let entry = materialCache[m.guid];
+  if (entry === undefined) {
+    const matFilePath = fileMap[m.guid];
+    const matFile = YAML.parse(fs.readFileSync(matFilePath, 'utf8'));
+    console.log('got material', matFile);
+    const textures = matFile.Material.m_SavedProperties.m_TexEnvs.map(o => {
+      const k = Object.keys(o);
+      o = o[k];
+      const {m_Texture} = o;
+      if (m_Texture.guid) {
+        const textureFilePath = fileMap[m_Texture.guid];
+        return textureFilePath || null;
+      } else {
+        return null;
+      }
+    }).filter(m => m !== null);
+    entry = textures;
+    materialCache[m.guid] = entry;
+  }
+  return entry;
 };
 const _collectComponents = o => {
   const result = [];
@@ -265,7 +284,7 @@ const _parseComponent = c => {
     }
     case 'MeshRenderer': {
       const {m_Materials} = component;
-      const materials = m_Materials.map(m => _parseMaterial(m));
+      const materials = m_Materials.map(m => _getMaterial(m));
       /* const gameObject = scene.find(o => o.fileID === component.m_GameObject.fileID);
       const components = gameObject.m_Component.map(c => {
         if (c.component.fileID !== fileID) {
@@ -280,10 +299,8 @@ const _parseComponent = c => {
     }
     case 'SkinnedMeshRenderer': {
       const {m_Mesh, m_Materials} = component;
-      const meshFilePath = fileMap[m_Mesh.guid];
-      const meshFile = YAML.parse(fs.readFileSync(meshFilePath, 'utf8'));
-      const mesh = _parseMesh(meshFile);
-      const materials = m_Materials.map(mat => _parseMaterial(mat));
+      const mesh = _getMesh(m_Mesh.guid);
+      const materials = m_Materials.map(m => _getMaterial(m));
       // console.log('got mat', JSON.stringify(materials, null, 2));
       // m_Mesh.fileID
       /* m_Mesh: {
@@ -306,26 +323,9 @@ const _parseComponent = c => {
     }
     case 'MeshFilter': {
       const {m_Mesh, m_Materials} = component;
-      const meshFilePath = fileMap[m_Mesh.guid];
-      /* const gameObject = scene.find(o => o.fileID === component.m_GameObject.fileID);
-      const components = gameObject.m_Component.map(c => {
-        if (c.component.fileID !== fileID) {
-          return _parseComponent(c);
-        } else {
-          return null;
-        }
-      }).filter(c => c !== null); */
-      
-      // console.log('got mesh filter', m_Mesh, meshFilePath);
-
-      if (meshFilePath) {
-        const meshFile = YAML.parse(fs.readFileSync(meshFilePath, 'utf8'));
-        const mesh = _parseMesh(meshFile);
-        console.log('got mesh', component);
-        return null; // XXX
-      } else {
-        return null;
-      }
+      const mesh = _getMesh(m_Mesh.guid);
+      console.log('got mesh', component);
+      return null; // XXX
     }
     default: {
       console.warn('unknown component', type);
